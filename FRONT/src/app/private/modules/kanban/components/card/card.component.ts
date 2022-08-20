6c886e88e6b22f7ccd84648d3853fcbab8730cfc
Feitoza de Azevedo <wrfv@gft.com>
2022-08-20 20:12:37 -0300

    feat: mostra a lista nos cards

--- FRONT/src/app/private/modules/kanban/components/card/card.component.ts
+++ FRONT/src/app/private/modules/kanban/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core'
+import { CardModel, listCarPerType } from '../../models/card.model'
 
 @Component({
   selector: 'app-card',
@@ -6,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core'
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  @Input() listCard: any
+  @Input() listCard: CardModel[]
   constructor() { }
 
   ngOnInit(): void {
