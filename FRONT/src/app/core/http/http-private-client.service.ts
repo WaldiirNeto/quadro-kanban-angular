c59f807b8a47c1b61e011ad4f69c462407632e93
Feitoza de Azevedo <wrfv@gft.com>
2022-08-20 17:36:17 -0300

    feat - cria servico http privado

---
+++ FRONT/src/app/core/http/http-private-client.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { GlobalEnums } from '@shared/enums/global-enums.enum'
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpPrivateClientService {
+  constructor(private readonly _http: HttpClient) {
+  }
+
+  public get<T>(url: string): Observable<T> {
+    const token = localStorage.getItem(GlobalEnums.TOKEN_KANBAN)
+    return this._http.get<T>(url, { headers: { 'Authorization': `Bearer ${token}` } })
+  }
+
+  public put<T>(url: string, payload: {}): Observable<T> {
+    const token = localStorage.getItem(GlobalEnums.TOKEN_KANBAN)
+    return this._http.put<T>(url, { headers: { 'Authorization': `Bearer ${token}` } })
+  }
+}
--- FRONT/src/app/shared/helpers/api-router.helper.ts
+++ FRONT/src/app/shared/helpers/api-router.helper.ts
@@ -2,4 +2,5 @@ import { environment } from '@environments/environment'
 
 export default class API_URL {
     static readonly LOGIN = `${environment.API_URL}/login`;
+    static readonly LIST_CARDS = `${environment.API_URL}/cards`
 }
